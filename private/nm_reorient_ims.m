8872eb5a680eabf94d0cc88ed4e1699ff005a242
Paul Wright <32168177+pwrightkcl@users.noreply.github.com>
2021-04-06 15:59:23 +0100

    Keep labels in alignment

--- private/nm_reorient_ims.m
+++ private/nm_reorient_ims.m
@@ -4,10 +4,23 @@ fprintf('Reslicing voxel-to-world...')
 N    = numel(Nii{1});
 M    = cell(1,N);
 M(:) = {eye(4)};
+
 for n=1:N
     f         = Nii{1}(n).dat.fname;    
     f         = nm_reorient(f);
     Nii{1}(n) = nifti(f);
 end
+
+if numel(Nii) > 1
+    % Keep labels in alignment
+    for n=1:N
+        if n > numel(Nii{2}) || isempty(Nii{2}(n).dat), continue; end
+        
+        f         = Nii{2}(n).dat.fname;    
+        f         = nm_reorient(f,[],'',0);
+        Nii{2}(n) = nifti(f);
+    end    
+end
+
 fprintf('done!\n')
 %==========================================================================
